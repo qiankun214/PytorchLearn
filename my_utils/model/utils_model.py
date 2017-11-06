9178249746e9060c38d9d0277dd9b04c7f9c87fb
qiankun214 <qiankun96214@outlook.com>
2017-11-06 23:31:15 +0800

    add resnet cell

--- my_utils/model/utils_model.py
+++ my_utils/model/utils_model.py
@@ -60,3 +60,40 @@ class ContinuousLinear(pt.nn.Module):
 
     def forward(self, x):
         return self.fc(x)
+
+
+class ResNet_2DCell(pt.nn.Module):
+
+    def __init__(self, channel_list, kernel_list, fill_mode="fc",
+                 conv_gen=pt.nn.Conv2d, afunc=pt.nn.ReLU, **other):
+        super(ResNet_2DCell, self).__init__()
+        self.conv = ContinuousConv2d(channel_list, kernel_list, padding_list=None,
+                                     stride_list=None, conv=conv_gen, afunc=afunc, **other)
+        if channel_list[0] == channel_list[-1]:
+            self.x_handle = lambda x: x
+        else:
+            if fill_mode == "fc":
+                self.x_handle = self.fc_fill(channel_list[0], channel_list[-1])
+            else:
+                self.x_handle = self.zero_fill(
+                    channel_list[0], channel_list[-1])
+
+    def forward(self, x):
+        return self.conv(x) + self.x_handle(x)
+
+    def fc_fill(self, in_channel, out_channel):
+        self.fc = pt.nn.Conv2d(in_channel, out_channel, 1)
+        return self._fill_fc
+
+    def _fill_fc(self, x):
+        return self.fc(x)
+
+    def zero_fill(self, in_channel, out_channel):
+        self.zeros_channel = abs(out_channel - in_channel)
+        self.zeros = pt.nn.Parameter(pt.zeros(1, self.zeros_channel, 1, 1))
+        return self._fill_zero
+
+    def _fill_zero(self, x):
+        zero_size = list(x.size())
+        zero_size[1] = self.zeros_channel
+        return pt.cat([self.zeros.expand(zero_size), x], dim=1)
